4128afa5aa346628397fda23e033613dce299223
Ayon Paul <ayonpaul8906@gmail.com>
2025-04-25 00:03:09 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ import speech_recognition as sr
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"]) 
+CORS(app, origins=["http://localhost:5173","https://zen-cue.vercel.app/"]) 
 
 # Config
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
